f2357f57f9ba8c17b0675a2675121fe91366acc1
Jatin Sonani <163812641+JatinSonani@users.noreply.github.com>
2024-05-16 20:21:49 +0530

    Add files via upload

---
+++ Q-1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void reverseArray(int *arr, int size) 
+{
+    int *start = arr; 
+    int *end = arr + size - 1;
+
+    while (start < end) 
+	{
+        int temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() 
+{
+    int size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    reverseArray(arr, size);
+
+    printf("Reversed array elements:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+}
